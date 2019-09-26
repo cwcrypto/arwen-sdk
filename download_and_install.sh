613168b1d8e48ae1dd259b579f2dc20560b98f95
Nabeelperson <nabeel.younis@yahoo.com>
2019-09-26 18:53:30 -0400

    Update script to pull both builds

--- download_and_install.sh
+++ download_and_install.sh
@@ -1,9 +1,13 @@
-wget https://cwc-trading-app-releases.s3.amazonaws.com/arwen-api.tar
+wget https://cwc-trading-app-releases.s3.amazonaws.com/arwen-api-external-testnet.tar
 
-docker load < arwen-api.tar
+docker load < arwen-api-external-testnet.tar
+
+wget https://cwc-trading-app-releases.s3.amazonaws.com/arwen-api-external-mainnet.tar
+
+docker load < arwen-api-external-mainnet.tar
 
 docker volume create arwen-database
 
-rm arwen-api.tar
+rm arwen-api*.tar
 
 docker image ls
