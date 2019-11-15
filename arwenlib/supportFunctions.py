9ad2b3cf1e988f8044d70a9661d941d5e04225be
Nabeelperson <nabeel.younis@yahoo.com>
2019-11-15 14:55:12 -0500

    [SupportFunctions]  - Removed simple escrow state, now exposed full arwen protocol escrow states  - Commented about new escrow states  - Fixed bug in funding address shown in openUserEscrow.py

--- arwenlib/supportFunctions.py
+++ arwenlib/supportFunctions.py
@@ -40,10 +40,54 @@ class Blockchain(enum.Enum):
     UNKNOWN = 'UNKNOWN'
 
 class EscrowState(enum.Enum):
-    OPENING = 'OPENING'
+    # User has requested a new Exchange/User Escrow
+    # but it has not yet been accepted by the exchange
+    PENDING = 'PENDING'
+
+    # First state for an Exchange Escrow after being created
+    # Remains in this state until user has paid the fee for the escrow
+    UNPAID_FEE = 'UNPAID_FEE'
+
+    # Temporary trading state for a User Escrow while making
+    # a sendFee request (signing cashout) for an Exchange Escrow
+    PAYING_FEE = 'PAYING_FEE'
+    
+    # Exchange rejected request for escrow
+    # or the Escrow was canceled by the user/exchange
+    # or the Escrow expired without funding
+    REJECTED = 'REJECTED'
+    
+    # Waiting for the Escrow to be funded on the blockchain
+    # and to receive safe block count number of confirmations
+    UNCONFIRMED = 'UNCONFIRMED'
+    
+    # Escrow funding transaction is confirmed on the blockchain
+    # Trading is now allowed with this escrow
     OPEN = 'OPEN'
+    
+    # This escrow is currently being used as part of an uncompleted RFQ trade
     TRADING = 'TRADING'
+    
+    # A cooperative cashout tx has been signed by the hub and client
+    # or an escrow closing tx has been detected on the blockchain
+    # Waiting for safe block count confirmation of the closing tx
+    CLOSING = 'CLOSING'
+    
+    # Marks this escrow as expiring soon- cannot be used for trading.
+    # Should only be allowed to close an Escrow in EXPIRING
+    EXPIRING = 'EXPIRING'
+    
+    # The escrow has been closed (spent) by a transaction that
+    # already has safe block count confirmations
     CLOSED = 'CLOSED'
+    
+    # The escrow is placed into frozen state after it was part of a
+    # aborted RFQ trade. A User or Exchange Escrow in FROZEN cannot be
+    # used for trading and can only be cooperatively or unilaterally
+    # closed. The escrow only move out of FROZEN state when CLOSED
+    FROZEN = 'FROZEN'
+    
+    # Fallback state, if you see this please tell me
     UNKNOWN = 'UNKNOWN'
 
 class OrderState(enum.Enum):
